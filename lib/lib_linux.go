0dfe240dd8717e3a808377bd0c78c471e9897c94
flevin58 <flevin58@gmail.com>
2024-02-17 02:05:36 +0100

    Modified and tested command on debian linux

--- lib/lib_linux.go
+++ lib/lib_linux.go
@@ -2,10 +2,16 @@ package lib
 
 import (
 	"os/exec"
+	"strings"
 )
 
 // Returns the serial number of the linux computer
 func GetComputerSerialNumber() string {
-	out, _ := exec.Command("dmidecode", "-t", "system").Output()
-	return out
+	out, err := exec.Command("/usr/bin/sudo", "bash", "-c", "dmidecode -t system | grep \"Serial Number:\"").Output()
+	if err != nil {
+		return ""
+	}
+	outs := string(out)
+	index := strings.Index(outs, ":")
+	return strings.TrimSpace(outs[index+1:])
 }
